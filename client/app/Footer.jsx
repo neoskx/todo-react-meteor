5394d1bac6cdbfc98c8cb9c12fc173183932eb0e
Shaoke Xu <shaoxu@tibco.com>
2015-12-06 13:14:00 -0800

    Chnage Author inform

--- client/app/Footer.jsx
+++ client/app/Footer.jsx
@@ -3,7 +3,7 @@ Components.Footer = React.createClass({
         return(
             <footer id="info">
                 <p>Double-click to edit a todo</p>
-                <p>Created by Matthias Stumpp - <a href="http://github.com/MStumpp">GitHub</a>&nbsp;&nbsp;<a href="http://twitter.com/MatStumpp">Twitter</a></p>
+                <p>Created by Shaoke Xu - <a href="https://github.com/shaoke">GitHub</a>&nbsp;&nbsp;<a href="https://twitter.com/shaoke">Twitter</a></p>
                 <p><a href="http://todomvcapp.meteor.com">Meteor&amp;React TodoMVC</a></p>
                 <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
             </footer>
